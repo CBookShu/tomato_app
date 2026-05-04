62b616a1e65dca84abdb6caf0d0eec9380a77fcc
cbookshu <cbooksh@outlook.com>
2026-05-04 17:53:50 +0800

    chore: scaffold monorepo with core package

---
+++ packages/core/jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+    '!src/db/connection.ts',
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+};
+
+export default config;
