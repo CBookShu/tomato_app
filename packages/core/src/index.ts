0e289dca1496ef76e4738362cef338399377a6ee
cbookshu <cbooksh@outlook.com>
2026-05-04 18:51:46 +0800

    feat: add barrel exports and verify full test suite

---
+++ packages/core/src/index.ts
@@ -0,0 +1,29 @@
+// Types
+export type { TimerStatus, TimerEvent, TimerState, PomodoroConfig } from './types/timer.js';
+export { DEFAULT_POMODORO_CONFIG } from './types/timer.js';
+export type { TaskStatus, Task, TaskGroup, NewTask, NewTaskGroup } from './types/task.js';
+export { DEFAULT_GROUP_ID } from './types/task.js';
+export type { DailyStats, MonthlyStats } from './types/stats.js';
+
+// Pomodoro
+export { transition } from './pomodoro/state-machine.js';
+export { PomodoroTimer } from './pomodoro/timer.js';
+
+// Tasks
+export { addTaskAtPosition, reorderTasks, removeTaskFromOrder } from './tasks/sorting.js';
+export { TaskManager } from './tasks/task-manager.js';
+export type { ITaskRepository, ITaskGroupRepository } from './tasks/task-manager.js';
+
+// Stats
+export { computeDailyStats, computeWeeklyTrend, computeMonthlyStats } from './stats/calculator.js';
+
+// Utils
+export { generateId } from './utils/id-generator.js';
+export { getToday, getWeekRange, getMonthKey } from './utils/date-utils.js';
+
+// Database
+export { getDb, closeDb } from './db/connection.js';
+export { TaskRepository } from './db/task-repository.js';
+export { TaskGroupRepository } from './db/task-group-repository.js';
+export { StatsRepository } from './db/stats-repository.js';
+export { SettingsRepository } from './db/settings-repository.js';
